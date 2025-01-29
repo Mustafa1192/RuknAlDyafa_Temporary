fc1204861d54405d69eda250955a248403286928
Khan Mustafa <150673700+Mustafa1192@users.noreply.github.com>
2025-01-29 16:58:45 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: "export",
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     output: "export",
 
-};
+// };
+
+// export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
 export default nextConfig;
